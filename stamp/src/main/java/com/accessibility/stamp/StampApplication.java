05806af258d206602ba6c67eeecb3bf25106338c
Claudio Henrique <HacskBoy@hotmail.com>
2021-05-01 16:14:50 -0300

    add: initial files of API

---
+++ stamp/src/main/java/com/accessibility/stamp/StampApplication.java
@@ -0,0 +1,13 @@
+package com.accessibility.stamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StampApplication.class, args);
+	}
+
+}

61190b1123f318f65dd3fdfb7e56303be76cffcb
Claudio Henrique <HacskBoy@hotmail.com>
2021-05-18 23:06:02 -0300

    refactor: remove list from user

--- stamp/src/main/java/com/accessibility/stamp/repository/UserRepository.java
+++ stamp/src/main/java/com/accessibility/stamp/repository/UserRepository.java
@@ -4,8 +4,6 @@ import com.accessibility.stamp.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);

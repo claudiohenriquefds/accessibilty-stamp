5a278ebdd2da2b2f444698a122c30af7f395304e
Claudio Henrique <HacskBoy@hotmail.com>
2021-06-14 08:29:44 -0300

    refactor[front]: correction in structure

--- spa/src/services/api.js
+++ spa/src/services/api.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: 'http://localhost:8080',
 });
 
-api.interceptors.request.use(async config => {
+api.interceptors.request.use(async (config) => {
     const token = getToken();
 
-    if(token){
+    if (token) {
+        // eslint-disable-next-line no-param-reassign
         config.headers.Authorization = `Bearer ${token}`;
     }
 

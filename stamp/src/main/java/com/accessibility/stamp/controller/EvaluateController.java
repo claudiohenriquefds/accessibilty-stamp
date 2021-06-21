17002d78dc82e0ee2e87f7594167df48111510e9
Claudio Henrique <HacskBoy@hotmail.com>
2021-06-21 10:28:44 -0300

    refactor[api]: increments evaluation every 24 hours

--- stamp/src/main/java/com/accessibility/stamp/controller/EvaluateController.java
+++ stamp/src/main/java/com/accessibility/stamp/controller/EvaluateController.java
@@ -1,40 +1,41 @@
 package com.accessibility.stamp.controller;
 
-import com.accessibility.stamp.entity.LogsEntity;
+import com.accessibility.stamp.entity.QueueEntity;
 import com.accessibility.stamp.entity.SiteEntity;
-import com.accessibility.stamp.repository.LogsRepository;
+import com.accessibility.stamp.repository.QueueRepository;
 import com.accessibility.stamp.repository.SiteRepository;
-import com.accessibility.stamp.service.AccessMonitorService;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/evaluate")
 public class EvaluateController {
-
-    @Autowired
-    private LogsRepository logsRepository;
 
     @Autowired
     private SiteRepository siteRepository;
 
-    @GetMapping("/run")
-    public boolean evalue() throws IOException {
-        AccessMonitorService accessMonitorService = new AccessMonitorService();
-        LogsEntity logs = new LogsEntity();
+    @Autowired
+    private QueueRepository queueRepository;
 
+    @Scheduled(fixedRate = 60*60*24000000)// every 24 hours 86400000000 milliseconds
+    public boolean evalue(){
         List<SiteEntity> sites = siteRepository.findAll();
-        for(int i = 0; i < sites.toArray().length; i++){
-            logs.setLogs(accessMonitorService.getValidation(sites.get(i).getUrl()).toString());
-            int validation = sites.get(i).getValidations();
-            sites.get(i).setValidations(validation+1);
-            logsRepository.save(logs);
+
+        for (int i = 0; i < sites.toArray().length; i++){
+            QueueEntity queueEntityVerify = queueRepository.findByUrl(sites.get(i).getUrl());
+            if(queueEntityVerify == null) {
+                QueueEntity queueEntity = new QueueEntity();
+                queueEntity.setUrl(sites.get(i).getUrl());
+                queueRepository.save(queueEntity);
+            }else{
+                queueEntityVerify.setRun(true);
+                queueRepository.save(queueEntityVerify);
+            }
+
+            sites.get(i).setRunSubsites(true);
+            siteRepository.save(sites.get(i));
         }
 
         return true;

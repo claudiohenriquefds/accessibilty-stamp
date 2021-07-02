b17c8c1e6a77a6523890f0592811d4fd6560bc4c
Claudio Henrique <HacskBoy@hotmail.com>
2021-07-02 17:40:03 -0300

    feat[api]: add method to get list of logs

--- stamp/src/main/java/com/accessibility/stamp/repository/LogsRepository.java
+++ stamp/src/main/java/com/accessibility/stamp/repository/LogsRepository.java
@@ -4,7 +4,9 @@ import com.accessibility.stamp.entity.LogsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LogsRepository extends JpaRepository<LogsEntity, Long> {
-
+    List<LogsEntity> findLogsEntitiesBySiteIdAndSubsite(Long siteId, Boolean subsite);
 }

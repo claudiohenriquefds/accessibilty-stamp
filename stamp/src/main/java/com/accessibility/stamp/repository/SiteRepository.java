1063814716642d69c95b2155c963cabc06ab0602
Claudio Henrique <HacskBoy@hotmail.com>
2021-07-02 17:38:55 -0300

    feat[api]: add method to find by id

--- stamp/src/main/java/com/accessibility/stamp/repository/SiteRepository.java
+++ stamp/src/main/java/com/accessibility/stamp/repository/SiteRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SiteRepository extends JpaRepository<SiteEntity, Long> {
     SiteEntity findByUrl(String url);
+    SiteEntity findSiteEntityById(Long id);
 }

06c5848cd3bf9805ce223609a24347c3d60594f0
Claudio Henrique <HacskBoy@hotmail.com>
2021-07-02 17:37:12 -0300

    feat[api]: add new method to find by id

--- stamp/src/main/java/com/accessibility/stamp/repository/SubsiteRepository.java
+++ stamp/src/main/java/com/accessibility/stamp/repository/SubsiteRepository.java
@@ -5,7 +5,10 @@ import com.accessibility.stamp.entity.SubsiteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SubsiteRepository extends JpaRepository<SubsiteEntity, Long> {
     SubsiteEntity findSubsiteByUrl(String url);
+    List<SubsiteEntity> findBySiteId(Long site_id);
 }

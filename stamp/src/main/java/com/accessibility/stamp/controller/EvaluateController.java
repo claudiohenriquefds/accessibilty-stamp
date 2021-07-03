dbe8fdde9b142bd4a92cfafe5726886d286ab4d5
Claudio Henrique <HacskBoy@hotmail.com>
2021-07-03 13:11:28 -0300

    refactor[api]: add route and standardize code

--- stamp/src/main/java/com/accessibility/stamp/controller/EvaluateController.java
+++ stamp/src/main/java/com/accessibility/stamp/controller/EvaluateController.java
@@ -6,11 +6,14 @@ import com.accessibility.stamp.repository.QueueRepository;
 import com.accessibility.stamp.repository.SiteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/evaluate")
 public class EvaluateController {
 
     @Autowired
@@ -20,22 +23,23 @@ public class EvaluateController {
     private QueueRepository queueRepository;
 
     @Scheduled(fixedRate = 60*60*24000000)// every 24 hours 86400000000 milliseconds
+    @GetMapping
     public boolean evalue(){
-        List<SiteEntity> sites = siteRepository.findAll();
+        List<SiteEntity> siteList = siteRepository.findAll();
 
-        for (int i = 0; i < sites.toArray().length; i++){
-            QueueEntity queueEntityVerify = queueRepository.findByUrl(sites.get(i).getUrl());
+        for (int i = 0; i < siteList.toArray().length; i++){
+            QueueEntity queueEntityVerify = queueRepository.findByUrl(siteList.get(i).getUrl());
             if(queueEntityVerify == null) {
                 QueueEntity queueEntity = new QueueEntity();
-                queueEntity.setUrl(sites.get(i).getUrl());
+                queueEntity.setUrl(siteList.get(i).getUrl());
                 queueRepository.save(queueEntity);
             }else{
                 queueEntityVerify.setRun(true);
                 queueRepository.save(queueEntityVerify);
             }
 
-            sites.get(i).setRunSubsites(true);
-            siteRepository.save(sites.get(i));
+            siteList.get(i).setRunSubsites(true);
+            siteRepository.save(siteList.get(i));
         }
 
         return true;

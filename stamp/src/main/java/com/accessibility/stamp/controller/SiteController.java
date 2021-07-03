78e110b4f260e8cd9f179053cd0b8c92b91229a5
Claudio Henrique <HacskBoy@hotmail.com>
2021-07-03 13:12:58 -0300

    refactor[api]: adds new methods and refactors feedback

--- stamp/src/main/java/com/accessibility/stamp/controller/SiteController.java
+++ stamp/src/main/java/com/accessibility/stamp/controller/SiteController.java
@@ -34,44 +34,166 @@ public class SiteController {
     @Autowired
     private QueueRepository queueRepository;
 
-    @PostMapping("/save")
-    public boolean post(@RequestBody String url) throws JSONException, IOException {
-        JSONObject json = new JSONObject(url);
+    @PostMapping("/store")
+    public String store(@RequestBody String requestBody) throws JSONException, IOException {
+        JSONObject body = new JSONObject(requestBody);
+        JSONObject jsonResponse = new JSONObject();
 
-        SiteEntity site = new SiteEntity();
-        QueueEntity queue = new QueueEntity();
+        SiteEntity siteEntity = new SiteEntity();
+        QueueEntity queueEntity = new QueueEntity();
 
-        if(json.get("site").toString() != null){
-            site.setUrl(json.get("site").toString());
-            site.setName(json.get("name").toString());
-            queue.setUrl(json.get("site").toString());
-            queueRepository.save(queue);
-            siteRepository.save(site);
+        if(body.get("url").toString() != null){
+            try{
+                siteEntity.setUrl(body.get("url").toString());
+                siteEntity.setName(body.get("name").toString());
+                queueEntity.setUrl(body.get("url").toString());
+                queueRepository.save(queueEntity);
+                siteRepository.save(siteEntity);
 
-            return true;
+                jsonResponse.put("success", true);
+                jsonResponse.put("data", null);
+                jsonResponse.put("errors", null);
+            }catch(JSONException e){
+                jsonResponse.put("success", false);
+                jsonResponse.put("data", null);
+                jsonResponse.put("errors", e);
+            }
+        }else{
+            JSONArray errors = new JSONArray();
+            errors.put("Campos obrigatórios não foram informados.");
+
+            jsonResponse.put("success", false);
+            jsonResponse.put("data", null);
+            jsonResponse.put("errors", errors);
         }
 
-        return false;
+        return jsonResponse.toString();
     }
-    @GetMapping("/list")
-    public String list() throws JSONException {
-        List<SiteEntity> sites = siteRepository.findAll();
-        JSONArray result = new JSONArray();
+    @GetMapping("/show")
+    public String show() throws JSONException {
+        List<SiteEntity> siteList = siteRepository.findAll();
+        JSONObject jsonResponse = new JSONObject();
 
-        for(int i = 0; i < sites.toArray().length; i++){
-            JSONObject json = new JSONObject();
-            List<SubsiteEntity> subsiteEntities = subsiteRepository.findBySiteId(sites.get(i).getId());
-            StampEntity stampEntity = stampRepository.findByStampLevel(sites.get(i).getStampLevel());
-            json.put("name",sites.get(i).getName());
-            json.put("url",sites.get(i).getUrl());
-            json.put("last_score",sites.get(i).getLastScore());
-            json.put("average",sites.get(i).getAverage());
-            json.put("pages",subsiteEntities.toArray().length);
-            json.put("stamp", stampEntity.getImage());
-            result.put(json);
+        try{
+            JSONArray jsonData = new JSONArray();
+
+            for(int i = 0; i < siteList.toArray().length; i++){
+                JSONObject jsonDataSite = new JSONObject();
+                
+                List<SubsiteEntity> subsiteEntities = subsiteRepository.findBySiteId(siteList.get(i).getId());
+                StampEntity stampEntity = stampRepository.findByStampLevel(siteList.get(i).getStampLevel());
+                jsonDataSite.put("name",siteList.get(i).getName());
+                jsonDataSite.put("url",siteList.get(i).getUrl());
+                jsonDataSite.put("last_score",siteList.get(i).getLastScore());
+                jsonDataSite.put("average",siteList.get(i).getAverage());
+                jsonDataSite.put("pages",subsiteEntities.toArray().length);
+                jsonDataSite.put("stamp", stampEntity.getImage());
+                jsonData.put(jsonDataSite);
+            }
+
+            jsonResponse.put("success", true);
+            jsonResponse.put("data", jsonData.toString());
+            jsonResponse.put("errors", null);
+        }catch(JSONException e){
+            jsonResponse.put("success", false);
+            jsonResponse.put("data", null);
+            jsonResponse.put("errors", e);
         }
 
-        return result.toString();
+        return jsonResponse.toString();
+    }
+
+    @GetMapping("/get")
+    public String get(@RequestBody String requestBody) throws JSONException {
+        JSONObject body = new JSONObject(requestBody);
+        SiteEntity siteEntity = siteRepository.findSiteEntityById(Long.parseLong(body.get("id").toString()));
+        JSONObject jsonResponse = new JSONObject();
+
+        try{
+            JSONObject jsonData = new JSONObject();
+
+            List<SubsiteEntity> subsiteEntities = subsiteRepository.findBySiteId(siteEntity.getId());
+            StampEntity stampEntity = stampRepository.findByStampLevel(siteEntity.getStampLevel());
+
+            jsonData.put("name",siteEntity.getName());
+            jsonData.put("url",siteEntity.getUrl());
+            jsonData.put("last_score",siteEntity.getLastScore());
+            jsonData.put("average",siteEntity.getAverage());
+            jsonData.put("pages",subsiteEntities.toArray().length);
+            jsonData.put("stamp", stampEntity.getImage());
+
+            jsonResponse.put("success", true);
+            jsonResponse.put("data", jsonData.toString());
+            jsonResponse.put("errors", null);
+        }catch(JSONException e){
+            jsonResponse.put("success", false);
+            jsonResponse.put("data", null);
+            jsonResponse.put("errors", e);
+        }
+
+        return jsonResponse.toString();
+    }
+
+    @PostMapping("/update")
+    public String update(@RequestBody String requestBody) throws JSONException, IOException {
+        JSONObject body = new JSONObject(requestBody);
+        JSONObject jsonResponse = new JSONObject();
+
+        SiteEntity siteEntity = siteRepository.findSiteEntityById(Long.parseLong(body.get("id").toString()));
+        QueueEntity queueEntity = new QueueEntity();
+
+        if(body.get("site").toString() != null){
+            try{
+                siteEntity.setUrl(body.get("url").toString());
+                siteEntity.setName(body.get("name").toString());
+                queueEntity.setUrl(body.get("url").toString());
+                queueRepository.save(queueEntity);
+                siteRepository.save(siteEntity);
+
+                jsonResponse.put("success", true);
+                jsonResponse.put("data", null);
+                jsonResponse.put("errors", null);
+            }catch(JSONException e){
+                jsonResponse.put("success", false);
+                jsonResponse.put("data", null);
+                jsonResponse.put("errors", e);
+            }
+        }else{
+            JSONArray errors = new JSONArray();
+            errors.put("Campos obrigatórios não foram informados.");
+
+            jsonResponse.put("success", false);
+            jsonResponse.put("data", null);
+            jsonResponse.put("errors", errors);
+        }
+
+        return jsonResponse.toString();
+    }
+
+    @PostMapping("/delete")
+    public String delete(@RequestBody String requestBody) throws JSONException, IOException {
+        JSONObject body = new JSONObject(requestBody);
+        JSONObject jsonResponse = new JSONObject();
+
+        SiteEntity siteEntity = siteRepository.findSiteEntityById(Long.parseLong(body.get("id").toString()));
+        QueueEntity queueEntity = queueRepository.findByUrl(siteEntity.getUrl());
+
+        try{
+            if(queueEntity != null){
+                queueRepository.delete(queueEntity);
+            }
+            siteRepository.delete(siteEntity);
+
+            jsonResponse.put("success", true);
+            jsonResponse.put("data", null);
+            jsonResponse.put("errors", null);
+        }catch(JSONException e){
+            jsonResponse.put("success", false);
+            jsonResponse.put("data", null);
+            jsonResponse.put("errors", e);
+        }
+
+        return jsonResponse.toString();
     }
 
 }

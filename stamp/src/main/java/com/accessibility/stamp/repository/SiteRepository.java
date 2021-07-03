e2de542d8c19d8f694e6c15f66db00a294b330d5
Claudio Henrique <HacskBoy@hotmail.com>
2021-07-03 16:56:28 -0300

    refactor[api]: increments new method

--- stamp/src/main/java/com/accessibility/stamp/repository/SiteRepository.java
+++ stamp/src/main/java/com/accessibility/stamp/repository/SiteRepository.java
@@ -4,8 +4,12 @@ import com.accessibility.stamp.entity.SiteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SiteRepository extends JpaRepository<SiteEntity, Long> {
     SiteEntity findByUrl(String url);
+    SiteEntity findSiteEntityByIdAndUserId(Long id, Long userId);
     SiteEntity findSiteEntityById(Long id);
+    List<SiteEntity> findAllByUserId(Long id);
 }

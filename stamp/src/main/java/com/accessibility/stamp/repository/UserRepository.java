fc427b65b3a9ea4b4a69a758a5d1bb3fd2eeb083
Claudio Henrique <HacskBoy@hotmail.com>
2021-07-03 16:56:51 -0300

    refactor[api]: increments new method

--- stamp/src/main/java/com/accessibility/stamp/repository/UserRepository.java
+++ stamp/src/main/java/com/accessibility/stamp/repository/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
+    UserEntity findByToken(String token);
 }

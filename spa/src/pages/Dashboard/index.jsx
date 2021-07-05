74f78545b5e329e1c4ed6f6d49c00f8c3fadbd25
Claudio Henrique <HacskBoy@hotmail.com>
2021-07-04 22:20:53 -0300

    refactor[spa]: remove unused class

--- spa/src/pages/Dashboard/index.jsx
+++ spa/src/pages/Dashboard/index.jsx
@@ -3,20 +3,18 @@ import { Switch, Route } from 'react-router-dom';
 
 import Panel from './Panel';
 import History from './History';
-import Sites from './Sites';
-import Stamp from './Stamp';
+import Sites from './Sites/index';
 
 function Dashboard(props) {
-  const { match } = props;
+    const { match } = props;
 
-  return (
-    <Switch>
-      <Route path={`${match.path}/panel`} component={Panel} />
-      <Route path={`${match.path}/history`} component={History} />
-      <Route path={`${match.path}/sites`} component={Sites} />
-      <Route path={`${match.path}/stamp`} component={Stamp} />
-    </Switch>
-  );
+    return (
+        <Switch>
+            <Route path={`${match.path}/panel`} component={Panel} />
+            <Route path={`${match.path}/history`} component={History} />
+            <Route path={`${match.path}/sites`} component={Sites} />
+        </Switch>
+    );
 }
 
 export default Dashboard;

7d76be641fe51da3403150767d90b028f74a88b9
Cláudio Henrique <HacskBoy@hotmail.com>
2021-08-15 20:18:06 -0300

    refactor[spa]: increments try catch to validate jwt token

--- spa/src/services/auth.js
+++ spa/src/services/auth.js
@@ -1,7 +1,13 @@
 import jwt from 'jsonwebtoken';
 
 export const TOKEN_KEY = 'access_token';
-export const isAuthenticated = () => jwt.decode(localStorage.getItem(TOKEN_KEY), {complete: true}).payload.exp < (new Date()).getTime();
+export const isAuthenticated = () => {
+    try{
+        return jwt.decode(localStorage.getItem(TOKEN_KEY), {complete: true}).payload.exp < (new Date()).getTime();
+    }catch({ message }){
+        return false;
+    }
+}
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = (token) => {
     localStorage.setItem(TOKEN_KEY, token);

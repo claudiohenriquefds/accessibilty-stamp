64f0a562b5206424eb0a865bbc27f8927ae88d16
Cláudio Henrique <HacskBoy@hotmail.com>
2021-08-15 15:26:14 -0300

    refactor[api]: correction of dashboard data

--- stamp/src/main/java/com/accessibility/stamp/repository/HistoryRepository.java
+++ stamp/src/main/java/com/accessibility/stamp/repository/HistoryRepository.java
@@ -12,7 +12,7 @@ public interface HistoryRepository extends JpaRepository<HistoryEntity, Long> {
     @Query(value = "SELECT * FROM history WHERE site_id = ?1 AND DATE_FORMAT(created_at, '%m') = ?2", nativeQuery = true)
     List<HistoryEntity> findBySiteIdAndAndCreatedAt_Month(Long siteId, Integer month);
 
-    @Query(value = "SELECT  1 AS id, 1 AS score, 1 AS site_id, 1 AS status, history_data.average, concat('2021-', history_data.created_at, '-01') as created_at FROM (SELECT  AVG(score) AS average, DATE_FORMAT(created_at, '%m') AS created_at FROM history WHERE DATE_FORMAT(created_at, '%Y') = DATE_FORMAT(NOW(), '%Y') GROUP BY DATE_FORMAT(created_at, '%m')) AS history_data", nativeQuery = true)
+    @Query(value = "SELECT  1 AS id, 1 AS score, 1 AS site_id, 1 AS status, history_data.average, history_data.created_at as created_at FROM (SELECT  AVG(score) AS average, DATE_FORMAT(created_at, '%m') AS created_at FROM history WHERE DATE_FORMAT(created_at, '%Y') = DATE_FORMAT(NOW(), '%Y') GROUP BY DATE_FORMAT(created_at, '%m')) AS history_data", nativeQuery = true)
     List<HistoryEntity> findAllUsingRawQuery(Long siteId);
 
     List<HistoryEntity> findBySiteId(Long siteId);

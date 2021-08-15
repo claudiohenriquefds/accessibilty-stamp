72bf92026f601e0ae897267fe758b8c3a03755c2
Cláudio Henrique <HacskBoy@hotmail.com>
2021-08-15 19:55:57 -0300

    refactor[spa]: increment expiration validation to jwt

--- spa/src/services/auth.js
+++ spa/src/services/auth.js
@@ -1,5 +1,9 @@
+import jwt from 'jsonwebtoken';
+
+require('dotenv').config();
+
 export const TOKEN_KEY = 'access_token';
-export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+export const isAuthenticated = () => jwt.decode(localStorage.getItem(TOKEN_KEY), {complete: true}).payload.exp < (new Date()).getTime();
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = (token) => {
     localStorage.setItem(TOKEN_KEY, token);

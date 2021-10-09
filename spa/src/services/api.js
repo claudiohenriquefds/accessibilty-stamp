09607cbfc9fef83fd10f74ab96e9394e1b1afe3d
Cláudio Henrique <HacskBoy@hotmail.com>
2021-10-09 13:59:31 -0300

    refactor[spa]: config endpoint of request

--- spa/src/services/api.js
+++ spa/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-    baseURL: 'http://34.69.36.49:8080',
+    baseURL: 'http://localhost:8080',
 });
 
 api.interceptors.request.use(async (config) => {

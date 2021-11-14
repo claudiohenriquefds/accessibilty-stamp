ec2ed8e79824b964d06bb226cafd2f680749027c
Cláudio Henrique <HacskBoy@hotmail.com>
2021-11-14 17:45:00 -0300

    feat[api]: adding routes for data control

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,19 +1,31 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\DataController;
+use App\Http\Controllers\Api\HistoryController;
+use App\Http\Controllers\Api\SiteController;
+use App\Http\Controllers\Api\StampController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [UserController::class, 'doLogin']);
+Route::get('/logout', [UserController::class, 'doLogout']);
+Route::post('/register', [UserController::class, 'doRegister']);
+
+Route::get('/stamp', [StampController::class, 'show'])->middleware('cors');
+Route::get('/stamp/info/{site_id}', [StampController::class, 'info']);
+
+Route::get('/comparative/gov', [DataController::class, 'comparativeGov']);
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::group(['prefix' => 'site'], function(){
+        Route::post('/', [SiteController::class, 'store']);
+        Route::get('/', [SiteController::class, 'index']);
+        Route::get('/{site_id}', [SiteController::class, 'show']);
+        Route::get('/details/{site_id}', [SiteController::class, 'getDetailed']);
+    });
+
+    Route::get('/history/{site_id}', [HistoryController::class, 'show']);
+
+    Route::get('/data/{site_id}', [DataController::class, 'show']);
 });

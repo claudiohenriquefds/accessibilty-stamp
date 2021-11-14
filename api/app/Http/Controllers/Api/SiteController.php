bc175295dd0ce340b3f499b212b74b0d25534f31
Cláudio Henrique <HacskBoy@hotmail.com>
2021-11-14 17:39:36 -0300

    refactor[api]: adds method to get details and improves data handling

--- api/app/Http/Controllers/Api/SiteController.php
+++ api/app/Http/Controllers/Api/SiteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Site\SiteAddRequest;
+use App\Http\Services\Util\PaginateService;
 use App\Jobs\Evaluate;
 use App\Models\Site;
 use Illuminate\Support\Facades\Auth;
@@ -12,7 +13,42 @@ class SiteController extends Controller
 {
     public function index(){
         try{
-            return response()->json(['success' => true, 'error' => null, 'data' => Site::where('user_id', Auth::user()->id)->get()], 200);
+            $sites = Site::where('user_id', Auth::user()->id)->get();
+
+            collect($sites)->map(function($site){
+                $site['stamp'] = (new StampController())->show($site, request());
+                $site['pages'] = $site->subsites()->count();
+                return $site;
+            });
+
+            return response()->json([
+                'success' => true,
+                'error' => null,
+                'data' => $sites ?? null
+            ], 200);
+        }catch(\Exception $exception){
+            return response()->json([
+                'success' => false,
+                'error' => [
+                    'file' => $exception->getFile(),
+                    'line' => $exception->getLine(),
+                    'message' => $exception->getMessage(),
+                    'trace' => $exception->getTraceAsString()
+                ],
+                'data' => null
+            ], 400);
+        }
+    }
+
+    public function show($id){
+        try{
+            $site = Site::find($id);
+
+            return response()->json([
+                'success' => true,
+                'error' => null,
+                'data' => $site ?? null
+            ], 200);
         }catch(\Exception $exception){
             return response()->json([
                 'success' => false,
@@ -37,6 +73,37 @@ class SiteController extends Controller
             Evaluate::dispatch($site);
 
             return response()->json(['success' => true, 'error' => null, 'data' => null], 200);
+        }catch(\Exception $exception){
+            return response()->json([
+                'success' => false,
+                'error' => [
+                    'file' => $exception->getFile(),
+                    'line' => $exception->getLine(),
+                    'message' => $exception->getMessage(),
+                    'trace' => $exception->getTraceAsString()
+                ],
+                'data' => null
+            ], 400);
+        }
+    }
+
+    public function getDetailed($id){
+        try{
+            $site = Site::find($id);
+            if(!is_null($site)){
+                $details = $site->detail;
+
+                collect($details)->map(function($detail){
+                    $detail['elements_detailed'] = $detail->detailElements;
+                    return $detail;
+                });
+            }
+
+            return response()->json([
+                'success' => true,
+                'error' => null,
+                'data' => PaginateService::doPaginate(!is_null($site) ? $details : [], 10)
+            ], 200);
         }catch(\Exception $exception){
             return response()->json([
                 'success' => false,

50cd7253428d2d5bc4446d40d744e1882b5601c4
Cláudio Henrique <HacskBoy@hotmail.com>
2021-11-14 17:38:57 -0300

    refactor[api]: add pagination to history

--- api/app/Http/Controllers/Api/HistoryController.php
+++ api/app/Http/Controllers/Api/HistoryController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Services\Util\PaginateService;
 use App\Models\History;
 
 class HistoryController extends Controller
 {
     public function show($site_id){
         try{
-            return response()->json(['success' => true, 'error' => null, 'data' => History::where('site_id', $site_id)->paginate(10)], 200);
+            return response()->json([
+                'success' => true,
+                'error' => null,
+                'data' => PaginateService::doPaginate(History::where('site_id', $site_id)
+                    ->join('sites as s', 's.id', '=', 'histories.site_id')
+                    ->select('s.name', 's.url', 'histories.score', 'histories.average', 'histories.created_at', 'histories.status')
+                    ->get()->toArray(), 10)
+            ], 200);
         }catch(\Exception $exception){
             return response()->json([
                 'success' => false,

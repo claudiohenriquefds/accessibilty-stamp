55ebedf009c02419bf46aaa3e642cbdbd4c9ff43
Cláudio Henrique <HacskBoy@hotmail.com>
2021-11-18 08:31:13 -0300

    refactor[spa]: changes to delete site

--- spa/src/pages/Dashboard/Sites/List/index.jsx
+++ spa/src/pages/Dashboard/Sites/List/index.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable react/no-danger */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment, useRef } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+
+import { TrashIcon, ExclamationIcon } from '@heroicons/react/outline';
 
 import { Link, useHistory } from 'react-router-dom';
 import api from '../../../../services/api';
@@ -11,30 +14,48 @@ const Site = () => {
     const [sites, setSites] = useState([]);
     const history = useHistory();
 
+    const [open, setOpen] = useState(false);
+    const [siteDelete, setSiteDelete] = useState();
+
+    const cancelButtonRef = useRef(null);
+
     async function getSites() {
         const sitesArray = [];
-        api.get('site').then((response) => {
-            if (response.data.success) {
-                const {data} = response.data;
-                // eslint-disable-next-line array-callback-return
-                data.map((element) => {
-                    sitesArray.push({
-                        id: element.id,
-                        name: element.name,
-                        last_score: element.last_score,
-                        grade_average: element.average,
-                        pages: element.pages,
-                        url: element.url,
-                        stamp: element.stamp
+        api.get('site')
+            .then((response) => {
+                if (response.data.success) {
+                    const { data } = response.data;
+                    // eslint-disable-next-line array-callback-return
+                    data.map((element) => {
+                        sitesArray.push({
+                            id: element.id,
+                            name: element.name,
+                            last_score: element.last_score,
+                            grade_average: element.average,
+                            pages: element.pages,
+                            url: element.url,
+                            stamp: element.stamp,
+                        });
                     });
-                });
-            }
+                }
 
-            setSites(sitesArray);
-        }).catch((err) => {
-            console.log(err);
-            logout(history)
-        });
+                setSites(sitesArray);
+            })
+            .catch((err) => {
+                console.log(err);
+                logout(history);
+            });
+    }
+
+    async function deleteSite() {
+        api.delete(`site/${siteDelete}`)
+            .then(() => {
+                window.location.reload();
+            })
+            .catch((err) => {
+                console.log(err);
+                logout(history);
+            });
     }
 
     useEffect(() => {
@@ -43,7 +64,91 @@ const Site = () => {
 
     return (
         <>
-            <Navbar current="sites" endpoint="site"/>
+            <Transition.Root show={open} as={Fragment}>
+                <Dialog
+                    as="div"
+                    className="fixed z-10 inset-0 overflow-y-auto"
+                    initialFocus={cancelButtonRef}
+                    onClose={setOpen}
+                >
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        </Transition.Child>
+
+                        <span
+                            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                            aria-hidden="true"
+                        >
+                            &#8203;
+                        </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                    <div className="sm:flex sm:items-start">
+                                        <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                            <ExclamationIcon
+                                                className="h-6 w-6 text-red-600"
+                                                aria-hidden="true"
+                                            />
+                                        </div>
+                                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                            <Dialog.Title
+                                                as="h3"
+                                                className="text-lg leading-6 font-medium text-gray-900"
+                                            >
+                                                Remoção de site
+                                            </Dialog.Title>
+                                            <div className="mt-2">
+                                                <p className="text-sm text-gray-500">
+                                                    Você tem certeza que deseja remover esse site? Todos os dados serão removidos. Essa ação não poderá ser desfeita.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                                    <button
+                                        type="button"
+                                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                                        onClick={() => {
+                                            setOpen(false)
+                                            deleteSite(siteDelete)
+                                        }}
+                                    >
+                                        Deletar
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                        onClick={() => setOpen(false)}
+                                        ref={cancelButtonRef}
+                                    >
+                                        Cancelar
+                                    </button>
+                                </div>
+                            </div>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition.Root>
+            <Navbar current="sites" endpoint="site" />
             <div className="grid grid-cols-1 md:grid-cols-1">
                 <div className="flex flex-col m-3">
                     <div className="w-full flex flex-row-reverse">
@@ -139,7 +244,27 @@ const Site = () => {
                                                     </div>
                                                 </td>
                                                 <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                    <div className="flex-shrink-0" dangerouslySetInnerHTML={{__html: site.stamp}} />
+                                                    <div
+                                                        className="flex-shrink-0"
+                                                        dangerouslySetInnerHTML={{
+                                                            __html: site.stamp,
+                                                        }}
+                                                    />
+                                                </td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                    <button
+                                                        type="button"
+                                                        value={siteDelete}
+                                                        onClick={() => {
+                                                            setSiteDelete(site.id);
+                                                            setOpen(true);
+                                                        }}
+                                                    >
+                                                        <TrashIcon
+                                                            className="h-5 w-5 hover:text-red-500 outline-none"
+                                                            aria-hidden="true"
+                                                        />
+                                                    </button>
                                                 </td>
                                             </tr>
                                         ))}
